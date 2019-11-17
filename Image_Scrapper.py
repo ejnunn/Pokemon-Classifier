a69538025e7d77568f1853c5ffcec24bc59be0b4
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2019-11-17 13:57:05 -0800

    Added Squirtle and Bulbasaur images

--- Image_Scrapper.py
+++ Image_Scrapper.py
@@ -1,29 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib
+import urllib.request
 import cv2
+import numpy as np
 
-URL = 'https://www.google.com/search?tbm=isch&sxsrf=ACYBGNSAHOMDD_W4tUseuooQsWVIYdlEbA%3A1572927237445&source=hp&biw=1440&bih=711&ei=BffAXb2UGPSU0PEPhe27QA&q=pikachu+cartoon&oq=pikachu+cartoon&gs_l=img.3..0l10.1500.4906..5596...0.0..0.172.1225.11j4......0....1..gws-wiz-img.......35i39.k4dxc89N3uw&ved=0ahUKEwj9qvjJmtLlAhV0CjQIHYX2DggQ4dUDCAY&uact=5'
+# name of the pokemon we are getting images for
+POKEMON_NAME = 'Squirtle'
+MAX_IMAGES = 5
+
+# Pikachu Google Image Search
+URL = 'https://www.google.com/search?q=squirtle&rlz=1C5CHFA_enUS814US814&sxsrf=ACYBGNSpBufdU77j-MUSKmN8cS_plt6I6Q:1573446076162&source=lnms&tbm=isch&sa=X&ved=0ahUKEwijj8Wzp-HlAhXfIjQIHZm1D-YQ_AUIEigB&biw=651&bih=648'
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content,'html5lib')
-image_results = soup.find('div', {'id':'search'})
 
+# Gets the HTML code within the image results container
+search_results = soup.find('div', {'id':'search'})
 
-# name of the pokemon we are getting images for
-pokemon_name = 'Pikachu'
+#print(search_results.prettify())
 
-count = 0
 # loop through all images in results
-for img in image_results.find_all('img'):
-	# get image url link
-	img_url = img.get('src')
-	print(img_url)
+count = 0
+for result in search_results.find_all('img')[:MAX_IMAGES]:
+    try:
+        # get image url link
+        img_url = result.get('src')
+        print(img_url)
+        request = urllib.request.Request(img_url)
+        response = urllib.request.urlopen(request)
+        binary_str = response.read()
+        byte_array = bytearray(binary_str)
+        numpy_array = np.asarray(byte_array, dtype="uint8")
+        image = cv2.imdecode(numpy_array, cv2.IMREAD_UNCHANGED)
+        filename = '/dataset/%s/scrapped_%d.jpg' % (POKEMON_NAME, count)
+        cv2.imwrite(filename, image)
+        print('Saved image %d' % count)
+        count += 1
+    except Exception as e:
+        print(str(e))
 
-	filename = pokemon_name + str(count)
-	
-	cv2.imwrite('/dataset/' + filename + 'jpg', img_url)
-	
-	count += 1
-
+print("DONE")
 print('Downloaded {} images.'.format(count))
+
+
+verify_image = cv2.imread('dataset/Squirtle/scrapped_0.jpg', 0)
+cv2.imshow('Image', verify_image)
